1c1180d954dba295645f0a8dbe7fc45f830076b7
uccu1478 <uccu1478@gmail.com>
2022-10-26 00:08:33 +0800

    20221025
    layout

---
+++ index.html.js
@@ -0,0 +1,8 @@
+$(function () {
+    $("#showAllCollapse").click(function () {
+        $('.collapse').collapse('show');
+    });
+    $("#hideAllCollapse").click(function () {
+        $('.collapse').collapse('hide');
+    });
+});
