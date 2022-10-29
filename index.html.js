502ecb753847302e88afa7cd2e6cf79beecdf880
uccu1478 <uccu1478@gmail.com>
2022-10-29 21:18:22 +0800

    fake SPA...

--- index.html.js
+++ index.html.js
@@ -1,11 +1,5 @@
 $(function () {
-    $("#showAllCollapse").click(function () {
-        $('.collapseList').collapse('show');
-    });
-    $("#hideAllCollapse").click(function () {
-        $('.collapseList').collapse('hide');
-    });
-
+    // navbar in mobile - collapse
     $("#navbarExpand").click(function () {
         console.log($("#navbarExpand").hasClass('collapsed'));
         if ($("#navbarExpand").hasClass('collapsed')) {
@@ -22,4 +16,47 @@ $(function () {
         $('#navbarExpandIcon').text('keyboard_arrow_down');
         $('#navbarExpandText').text('展開');
     });
+
+    // main list - Expand & Collapse
+    $("#showAllCollapse").click(function () {
+        $('.collapseList').collapse('show');
+    });
+    $("#hideAllCollapse").click(function () {
+        $('.collapseList').collapse('hide');
+    });
+
+    // Copy in modal
+    $('#share').click(function () {
+        let copyUrl = `${window.location.pathname}?p=${$('#path').val()}&f=${$('#file').val()}`;
+        copy(copyUrl)
+    });
+
+    // IF url has p and f params, open modal
+    let searchParams = new URLSearchParams(window.location.search);
+    if (searchParams.has('p') && searchParams.has('f')) {
+        const path = searchParams.get('p');
+        const file = searchParams.get('f');
+        if (path != '' && file != '') {
+            $('#path').val(path);
+            $('#file').val(file);
+            openModal();
+        }
+    }
+
+    // openModal click
+    $('.openModal').click(function () {
+        $('#path').val($(this).data('path'));
+        $('#file').val($(this).data('file'));
+        openModal();
+    });
 });
+
+function copy(text) {
+    navigator.clipboard.writeText(text);
+}
+
+function openModal() {
+    // try access html
+    $("#modalContent").load(`./${$('#path').val()}/${$('#file').val()}.html`);
+    $('#staticBackdrop').modal('show');
+}
