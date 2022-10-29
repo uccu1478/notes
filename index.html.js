5ad9d04419114e472100dbc0189dd18120b6f99e
uccu1478 <uccu1478@gmail.com>
2022-10-29 13:34:52 +0800

    icons nav-expand

--- index.html.js
+++ index.html.js
@@ -5,4 +5,21 @@ $(function () {
     $("#hideAllCollapse").click(function () {
         $('.collapseList').collapse('hide');
     });
+
+    $("#navbarExpand").click(function () {
+        console.log($("#navbarExpand").hasClass('collapsed'));
+        if ($("#navbarExpand").hasClass('collapsed')) {
+            $('#navbarExpandIcon').text('keyboard_arrow_down');
+            $('#navbarExpandText').text('展開');
+        }
+        else {
+            $('#navbarExpandIcon').text('keyboard_arrow_up');
+            $('#navbarExpandText').text('收合');
+        }
+    });
+    $(".collapseAfterClick").click(function () {
+        $('#navbarCollapse').removeClass("show");
+        $('#navbarExpandIcon').text('keyboard_arrow_down');
+        $('#navbarExpandText').text('展開');
+    });
 });
