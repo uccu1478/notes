960ad292a0104338bbdda11259832e6fd3cc991b
uccu1478 <uccu1478@gmail.com>
2025-01-23 11:59:30 +0800

    js module

--- index.html.js
+++ index.html.js
@@ -1,44 +1,4 @@
-const sections = [
-    {
-        id: 'CPlusPlus', icon: 'devicon-cplusplus-plain', title: 'C++', items: [
-            { text: 'C++單元測試', date: '2024-03-16', path: 'cpp', file: 'cpp-1' },
-        ]
-    },
-    {
-        id: 'CSharp', icon: 'devicon-csharp-plain', title: 'C#', items: [
-            { text: '檔案上傳 .NET 6 MVC', date: '2022-11-08', path: 'csharp', file: 'csharp-1' },
-            { text: '檔案上傳 WinForm 上傳PDF', date: '2023-02-12', path: 'csharp', file: 'csharp-2' },
-            { text: 'Blazor OCR', date: '2023-02-12', path: 'csharp', file: 'csharp-3' },
-            { text: 'Unity 物件移動、控制', date: '2024-07-09', path: 'csharp', file: 'csharp-4' },
-        ]
-    },
-    {
-        id: 'JS', icon: 'devicon-javascript-plain', title: 'JS', items: [
-            { text: 'VueSelect', date: '2023-02-12', path: 'js', file: 'js-1' },
-            { text: '動態嵌入Gist', date: '2024-03-14', path: 'js', file: 'js-2' },
-        ]
-    },
-    {
-        id: 'PY', icon: 'devicon-python-plain', title: 'Python', items: [
-            { text: 'Implementation of ResNet for Image Classification on the Flowers102 Dataset', date: '2025-01-22', path: 'py', file: 'py-1' },
-        ]
-    },
-    {
-        id: 'SQL', icon: 'devicon-microsoftsqlserver-plain', title: 'SQL', items: [
-            { text: '取得新增資料之識別-OUTPUT', date: '2022-11-04', path: 'sql', file: 'sql-1' },
-        ]
-    },
-    {
-        id: 'Z', icon: 'devicon-gitbook-original', title: '其他', items: [
-            { text: '上架App至Google Play Store', date: '2024-04-02', path: 'z', file: 'z-1' },
-            { text: 'Windows 10 工作列顯示秒數', date: '2024-04-03', path: 'z', file: 'z-2' },
-            { text: 'Windows 10 相片 應用程式消失', date: '2024-04-16', path: 'z', file: 'z-3' },
-            { text: '判斷Endian', date: '2024-04-26', path: 'z', file: 'z-4' },
-            // { text: '課程筆記-機器學習2021', date: '2024-08-30', path: 'z', file: 'z-5' },
-            { text: 'RV1126 連接WiFi網路', date: '2024-10-19', path: 'z', file: 'z-6' },
-        ]
-    },
-];
+import { sections } from './sections.js?v=20250123';
 
 $(function () {
     initSections();
@@ -70,7 +30,7 @@ function initSections() {
 
 function initClicks() {
     // navbar in mobile - collapse
-    $("#navbarExpand").click(function () {
+    $("#navbarExpand").on('click', function () {
         if ($("#navbarExpand").hasClass('collapsed')) {
             $('#navbarExpandIcon').text('keyboard_arrow_down');
             $('#navbarExpandText').text('展開');
@@ -80,35 +40,40 @@ function initClicks() {
             $('#navbarExpandText').text('收合');
         }
     });
-    $(".collapseAfterClick").click(function () {
+    $(".collapseAfterClick").on('click', function () {
         $('#navbarCollapse').removeClass("show");
         $('#navbarExpandIcon').text('keyboard_arrow_down');
         $('#navbarExpandText').text('展開');
     });
 
     // toptools
-    $("#showAllCollapse").click(function () {
+    $("#showAllCollapse").on('click', function () {
         $('.collapseList').collapse('show');
     });
-    $("#hideAllCollapse").click(function () {
+    $("#hideAllCollapse").on('click', function () {
         $('.collapseList').collapse('hide');
     });
 
     // Copy to clipboard in modal
-    $('#share').click(function () {
+    $('#share').on('click', function () {
         let copyUrl = `${window.location.origin}${window.location.pathname}?p=${$('#path').val()}&f=${$('#file').val()}`;
         copy(copyUrl);
     });
 
-    $('.openModal').click(function () {
+    $('.openModal').on('click', function () {
         $('#path').val($(this).data('path'));
         $('#file').val($(this).data('file'));
         openModal();
     });
 
-    $('#bug_report').click(function () {
+    $('#bug_report').on('click', function () {
         const subject = `bug_report:notes-${$('#file').val()}`;
         window.location = `mailto:uccu1478@gmail.com?subject=${subject}&body=Hello!`;
+    });
+
+    // Remove focus after button click
+    $('.btn').on('click', function () {
+        $(this).blur();
     });
 }
 
---
+++ sections.js
@@ -0,0 +1,40 @@
+export const sections = [
+    {
+        id: 'CPlusPlus', icon: 'devicon-cplusplus-plain', title: 'C++', items: [
+            { text: 'C++單元測試', date: '2024-03-16', path: 'cpp', file: 'cpp-1' },
+        ]
+    },
+    {
+        id: 'CSharp', icon: 'devicon-csharp-plain', title: 'C#', items: [
+            { text: '檔案上傳 .NET 6 MVC', date: '2022-11-08', path: 'csharp', file: 'csharp-1' },
+            { text: '檔案上傳 WinForm 上傳PDF', date: '2023-02-12', path: 'csharp', file: 'csharp-2' },
+            { text: 'Blazor OCR', date: '2023-02-12', path: 'csharp', file: 'csharp-3' },
+            { text: 'Unity 物件移動、控制', date: '2024-07-09', path: 'csharp', file: 'csharp-4' },
+        ]
+    },
+    {
+        id: 'JS', icon: 'devicon-javascript-plain', title: 'JS', items: [
+            { text: 'VueSelect', date: '2023-02-12', path: 'js', file: 'js-1' },
+            { text: '動態嵌入Gist', date: '2024-03-14', path: 'js', file: 'js-2' },
+        ]
+    },
+    {
+        id: 'PY', icon: 'devicon-python-plain', title: 'Python', items: [
+            { text: 'Implementation of ResNet for Image Classification on the Flowers102 Dataset', date: '2025-01-22', path: 'py', file: 'py-1' },
+        ]
+    },
+    {
+        id: 'SQL', icon: 'devicon-microsoftsqlserver-plain', title: 'SQL', items: [
+            { text: '取得新增資料之識別-OUTPUT', date: '2022-11-04', path: 'sql', file: 'sql-1' },
+        ]
+    },
+    {
+        id: 'Z', icon: 'devicon-gitbook-original', title: '其他', items: [
+            { text: '上架App至Google Play Store', date: '2024-04-02', path: 'z', file: 'z-1' },
+            { text: 'Windows 10 工作列顯示秒數', date: '2024-04-03', path: 'z', file: 'z-2' },
+            { text: 'Windows 10 相片 應用程式消失', date: '2024-04-16', path: 'z', file: 'z-3' },
+            { text: '判斷Endian', date: '2024-04-26', path: 'z', file: 'z-4' },
+            { text: 'RV1126 連接WiFi網路', date: '2024-10-19', path: 'z', file: 'z-6' },
+        ]
+    },
+];
